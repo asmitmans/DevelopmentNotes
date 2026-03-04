1bc76d65534dc6fcd0b4d211c1f245f119311702
asmitmans <a.smitmans@gmail.com>
2026-03-04 16:59:33 -0300

    feat(demo): recreate SpringSecurityJWTDemo baseline (Boot 4.0.2)

---
+++ ioTDataAPINotes/01_SpringSecurity/springsecurity-jwt-demo/src/test/java/com/demo/springsecurity_jwt_demo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springsecurity_jwt_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
